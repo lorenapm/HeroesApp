f0b5bc09cf189627008c4038e59c45860b7bf9d2
Lorena <lorena.perez@gmail.com>
2021-09-12 16:53:38 +0200

    add history push

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    //push: permite volver atrás en la pantalla de nav
+    //history.push("/");
+    //replace: no permite volver atras en la navegación
+    history.replace("/");
+  };
+
   return (
     <div>
-      <h1>Login Screen</h1>
+      <h1>Login</h1>
+      <hr />
+
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 };
