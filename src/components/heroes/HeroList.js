4d82ef214577c58a5787a80a5e95c928a004891f
Lorena <lorena.perez@gmail.com>
2021-09-13 13:16:36 +0200

    add smalls animates

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -5,7 +5,7 @@ import { HeroCard } from "./HeroCard";
 export const HeroList = ({ publisher }) => {
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
   return (
-    <div className="row">
+    <div className="row animate__animated animate__fadeIn">
       {heroes.map((hero) => (
         <HeroCard key={hero.id} {...hero} />
       ))}
