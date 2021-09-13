5551cf04e754588ad10bae3e973e701d4740b6f1
Lorena <lorena.perez@gmail.com>
2021-09-13 10:58:07 +0200

    create Hero Card

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -5,7 +5,6 @@ export const MarvelScreen = () => {
   return (
     <div>
       <h1>Marvel</h1>
-      <h2>List Marvel Heroes</h2>
       <hr />
       <HeroList publisher="Marvel Comics" />
     </div>
