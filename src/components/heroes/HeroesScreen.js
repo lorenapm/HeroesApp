8d839bf55b73f048ef2a398f4319785db1a4d395
Lorena <lorena.perez@gmail.com>
2021-09-13 12:53:41 +0200

    add Heroes Screen and style

--- src/components/heroes/HeroesScreen.js
+++ src/components/heroes/HeroesScreen.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Redirect, useParams } from "react-router";
 import { getHeroeById } from "../../selectors/getHeroById";
 
-export const HeroesScreen = () => {
+export const HeroesScreen = (props) => {
   const { heroeId } = useParams();
-  console.log(heroeId);
 
   const hero = getHeroeById(heroeId);
-  console.log(hero);
+
+  const handleReturn = () => {
+    if (props.history.length <= 2) {
+      props.history.push("/");
+    } else {
+      props.history.goBack();
+    }
+  };
 
   if (!hero) {
     return <Redirect to="/" />;
@@ -17,8 +23,34 @@ export const HeroesScreen = () => {
     hero;
 
   return (
-    <div>
-      <h1>HeroesScreen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          className="img-thumbnail"
+          src={`../assets/heroes/${heroeId}.jpg`}
+          alt={superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: {alter_ego}</b>
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: {publisher}</b>
+          </li>
+          <li className="list-group-item">
+            <b>First appearance: {first_appearance}</b>
+          </li>
+        </ul>
+        <h5>Characters</h5>
+        <p>{characters}</p>
+
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
